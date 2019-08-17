e53fdd893169388e081bfef6bbf953a310f9ecf2
Luis Rolando Vasquez Velasquez <vasquez.luisrolando@gmail.com>
2019-08-16 23:17:28 -0400

    P01.02: CelulaViva_conMenosDeDosVecinasVivas : Se creo el metodo devolviendo valor tal cual como se espera en la prueba.

--- JuegoDeLaVida/JuegoDeLaVidaLib/Juego.cs
+++ JuegoDeLaVida/JuegoDeLaVidaLib/Juego.cs
@@ -6,5 +6,21 @@ namespace JuegoDeLaVidaLib
 {
     public class Juego
     {
+        public bool[,] generar(bool[,] tableroBase, int posX, int posY)
+        {
+            bool[,] tablero = new bool[10, 10];
+            tablero[0, 0] = false; tablero[0, 0] = false; tablero[0, 0] = false; tablero[0, 0] = false; tablero[0, 0] = false; tablero[0, 0] = false; tablero[0, 0] = false; tablero[0, 0] = false; tablero[0, 0] = false; tablero[0, 0] = false;
+            tablero[0, 0] = false; tablero[0, 0] = false; tablero[0, 0] = false; tablero[0, 0] = false; tablero[0, 0] = false; tablero[0, 0] = false; tablero[0, 0] = false; tablero[0, 0] = false; tablero[0, 0] = false; tablero[0, 0] = false;
+            tablero[0, 0] = false; tablero[0, 0] = false; tablero[0, 0] = false; tablero[0, 0] = false; tablero[0, 0] = false; tablero[0, 0] = false; tablero[0, 0] = false; tablero[0, 0] = false; tablero[0, 0] = false; tablero[0, 0] = false;
+            tablero[0, 0] = false; tablero[0, 0] = false; tablero[0, 0] = false; tablero[0, 0] = false; tablero[0, 0] = false; tablero[0, 0] = false; tablero[0, 0] = false; tablero[0, 0] = false; tablero[0, 0] = false; tablero[0, 0] = false;
+            tablero[0, 0] = false; tablero[0, 0] = false; tablero[0, 0] = false; tablero[0, 0] = false; tablero[0, 0] = false; tablero[0, 0] = false; tablero[0, 0] = false; tablero[0, 0] = false; tablero[0, 0] = false; tablero[0, 0] = false;
+            tablero[0, 0] = false; tablero[0, 0] = false; tablero[0, 0] = false; tablero[0, 0] = false; tablero[0, 0] = false; tablero[0, 0] = false; tablero[0, 0] = false; tablero[0, 0] = false; tablero[0, 0] = false; tablero[0, 0] = false;
+            tablero[0, 0] = false; tablero[0, 0] = false; tablero[0, 0] = false; tablero[0, 0] = false; tablero[0, 0] = false; tablero[0, 0] = false; tablero[0, 0] = false; tablero[0, 0] = false; tablero[0, 0] = false; tablero[0, 0] = false;
+            tablero[0, 0] = false; tablero[0, 0] = false; tablero[0, 0] = false; tablero[0, 0] = false; tablero[0, 0] = false; tablero[0, 0] = false; tablero[0, 0] = false; tablero[0, 0] = false; tablero[0, 0] = false; tablero[0, 0] = false;
+            tablero[0, 0] = false; tablero[0, 0] = false; tablero[0, 0] = false; tablero[0, 0] = false; tablero[0, 0] = false; tablero[0, 0] = false; tablero[0, 0] = false; tablero[0, 0] = false; tablero[0, 0] = false; tablero[0, 0] = false;
+            tablero[0, 0] = false; tablero[0, 0] = false; tablero[0, 0] = false; tablero[0, 0] = false; tablero[0, 0] = false; tablero[0, 0] = false; tablero[0, 0] = false; tablero[0, 0] = false; tablero[0, 0] = false; tablero[0, 0] = false;
+
+            return tablero;
+        }
     }
 }
