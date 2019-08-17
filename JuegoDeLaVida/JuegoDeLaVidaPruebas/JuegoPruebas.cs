b053f4cc5220f771c302fd018c6f1362d216e8ad
Luis Rolando Vasquez Velasquez <vasquez.luisrolando@gmail.com>
2019-08-16 23:10:47 -0400

    P01: CelulaViva_conMenosDeDosVecinasVivas : Error al compilar, ya que no existe metodo generar en la clase Juego. Paso 1: generando error.

--- JuegoDeLaVida/JuegoDeLaVidaPruebas/JuegoPruebas.cs
+++ JuegoDeLaVida/JuegoDeLaVidaPruebas/JuegoPruebas.cs
@@ -7,11 +7,38 @@ namespace JuegoDeLaVidaPruebas
 {
     public class JuegoPruebas
     {
+
         [Test]
         public void CelulaViva_conMenosDeDosVecinasVivas()
         {
             JuegoDeLaVidaLib.Juego juego = new JuegoDeLaVidaLib.Juego();
-            Assert.AreEqual("","");
+            bool[,] tablero = new bool[10, 10];
+            tablero[0, 0] = false; tablero[0, 0] = false; tablero[0, 0] = false; tablero[0, 0] = false; tablero[0, 0] = false; tablero[0, 0] = false; tablero[0, 0] = false; tablero[0, 0] = false; tablero[0, 0] = false; tablero[0, 0] = false;
+            tablero[0, 0] = false; tablero[0, 0] = true; tablero[0, 0] = false; tablero[0, 0] = false; tablero[0, 0] = false; tablero[0, 0] = false; tablero[0, 0] = false; tablero[0, 0] = false; tablero[0, 0] = false; tablero[0, 0] = false;
+            tablero[0, 0] = false; tablero[0, 0] = false; tablero[0, 0] = false; tablero[0, 0] = false; tablero[0, 0] = false; tablero[0, 0] = false; tablero[0, 0] = false; tablero[0, 0] = false; tablero[0, 0] = false; tablero[0, 0] = false;
+            tablero[0, 0] = false; tablero[0, 0] = false; tablero[0, 0] = false; tablero[0, 0] = false; tablero[0, 0] = false; tablero[0, 0] = false; tablero[0, 0] = false; tablero[0, 0] = false; tablero[0, 0] = false; tablero[0, 0] = false;
+            tablero[0, 0] = false; tablero[0, 0] = false; tablero[0, 0] = false; tablero[0, 0] = false; tablero[0, 0] = false; tablero[0, 0] = false; tablero[0, 0] = false; tablero[0, 0] = false; tablero[0, 0] = false; tablero[0, 0] = false;
+            tablero[0, 0] = false; tablero[0, 0] = false; tablero[0, 0] = false; tablero[0, 0] = false; tablero[0, 0] = false; tablero[0, 0] = false; tablero[0, 0] = false; tablero[0, 0] = false; tablero[0, 0] = false; tablero[0, 0] = false;
+            tablero[0, 0] = false; tablero[0, 0] = false; tablero[0, 0] = false; tablero[0, 0] = false; tablero[0, 0] = false; tablero[0, 0] = false; tablero[0, 0] = false; tablero[0, 0] = false; tablero[0, 0] = false; tablero[0, 0] = false;
+            tablero[0, 0] = false; tablero[0, 0] = false; tablero[0, 0] = false; tablero[0, 0] = false; tablero[0, 0] = false; tablero[0, 0] = false; tablero[0, 0] = false; tablero[0, 0] = false; tablero[0, 0] = false; tablero[0, 0] = false;
+            tablero[0, 0] = false; tablero[0, 0] = false; tablero[0, 0] = false; tablero[0, 0] = false; tablero[0, 0] = false; tablero[0, 0] = false; tablero[0, 0] = false; tablero[0, 0] = false; tablero[0, 0] = false; tablero[0, 0] = false;
+            tablero[0, 0] = false; tablero[0, 0] = false; tablero[0, 0] = false; tablero[0, 0] = false; tablero[0, 0] = false; tablero[0, 0] = false; tablero[0, 0] = false; tablero[0, 0] = false; tablero[0, 0] = false; tablero[0, 0] = false;
+            // Hay una celula viva en la posicion 1,1.
+            bool[,] tableroActual = juego.generar(tablero, 1, 1);
+            bool[,] tableroEsperado = new bool[10, 10];
+            tableroEsperado[0, 0] = false; tableroEsperado[0, 0] = false; tableroEsperado[0, 0] = false; tableroEsperado[0, 0] = false; tableroEsperado[0, 0] = false; tableroEsperado[0, 0] = false; tableroEsperado[0, 0] = false; tableroEsperado[0, 0] = false; tableroEsperado[0, 0] = false; tableroEsperado[0, 0] = false;
+            tableroEsperado[0, 0] = false; tableroEsperado[0, 0] = false; tableroEsperado[0, 0] = false; tableroEsperado[0, 0] = false; tableroEsperado[0, 0] = false; tableroEsperado[0, 0] = false; tableroEsperado[0, 0] = false; tableroEsperado[0, 0] = false; tableroEsperado[0, 0] = false; tableroEsperado[0, 0] = false;
+            tableroEsperado[0, 0] = false; tableroEsperado[0, 0] = false; tableroEsperado[0, 0] = false; tableroEsperado[0, 0] = false; tableroEsperado[0, 0] = false; tableroEsperado[0, 0] = false; tableroEsperado[0, 0] = false; tableroEsperado[0, 0] = false; tableroEsperado[0, 0] = false; tableroEsperado[0, 0] = false;
+            tableroEsperado[0, 0] = false; tableroEsperado[0, 0] = false; tableroEsperado[0, 0] = false; tableroEsperado[0, 0] = false; tableroEsperado[0, 0] = false; tableroEsperado[0, 0] = false; tableroEsperado[0, 0] = false; tableroEsperado[0, 0] = false; tableroEsperado[0, 0] = false; tableroEsperado[0, 0] = false;
+            tableroEsperado[0, 0] = false; tableroEsperado[0, 0] = false; tableroEsperado[0, 0] = false; tableroEsperado[0, 0] = false; tableroEsperado[0, 0] = false; tableroEsperado[0, 0] = false; tableroEsperado[0, 0] = false; tableroEsperado[0, 0] = false; tableroEsperado[0, 0] = false; tableroEsperado[0, 0] = false;
+            tableroEsperado[0, 0] = false; tableroEsperado[0, 0] = false; tableroEsperado[0, 0] = false; tableroEsperado[0, 0] = false; tableroEsperado[0, 0] = false; tableroEsperado[0, 0] = false; tableroEsperado[0, 0] = false; tableroEsperado[0, 0] = false; tableroEsperado[0, 0] = false; tableroEsperado[0, 0] = false;
+            tableroEsperado[0, 0] = false; tableroEsperado[0, 0] = false; tableroEsperado[0, 0] = false; tableroEsperado[0, 0] = false; tableroEsperado[0, 0] = false; tableroEsperado[0, 0] = false; tableroEsperado[0, 0] = false; tableroEsperado[0, 0] = false; tableroEsperado[0, 0] = false; tableroEsperado[0, 0] = false;
+            tableroEsperado[0, 0] = false; tableroEsperado[0, 0] = false; tableroEsperado[0, 0] = false; tableroEsperado[0, 0] = false; tableroEsperado[0, 0] = false; tableroEsperado[0, 0] = false; tableroEsperado[0, 0] = false; tableroEsperado[0, 0] = false; tableroEsperado[0, 0] = false; tableroEsperado[0, 0] = false;
+            tableroEsperado[0, 0] = false; tableroEsperado[0, 0] = false; tableroEsperado[0, 0] = false; tableroEsperado[0, 0] = false; tableroEsperado[0, 0] = false; tableroEsperado[0, 0] = false; tableroEsperado[0, 0] = false; tableroEsperado[0, 0] = false; tableroEsperado[0, 0] = false; tableroEsperado[0, 0] = false;
+            tableroEsperado[0, 0] = false; tableroEsperado[0, 0] = false; tableroEsperado[0, 0] = false; tableroEsperado[0, 0] = false; tableroEsperado[0, 0] = false; tableroEsperado[0, 0] = false; tableroEsperado[0, 0] = false; tableroEsperado[0, 0] = false; tableroEsperado[0, 0] = false; tableroEsperado[0, 0] = false;
+            // Tablero esperado, la celula unica viva se deberia morir.
+
+            Assert.AreEqual(tableroEsperado, tableroActual);
         }
     }
 }
