d050756977261fb5819b6820e21ce871536ab06f
Luis Rolando Vasquez Velasquez <vasquez.luisrolando@gmail.com>
2019-08-16 23:27:49 -0400

    P01.03: CelulaViva_conMenosDeDosVecinasVivas : Refactorizacion : Se encontro que el codigo estaba mal inicializado el tablero. Se corrigio tanto en la clase juego como en las pruebas. Pruebas satisfactorias.

--- JuegoDeLaVida/JuegoDeLaVidaLib/Juego.cs
+++ JuegoDeLaVida/JuegoDeLaVidaLib/Juego.cs
@@ -8,18 +8,12 @@ namespace JuegoDeLaVidaLib
     {
         public bool[,] generar(bool[,] tableroBase, int posX, int posY)
         {
-            bool[,] tablero = new bool[10, 10];
-            tablero[0, 0] = false; tablero[0, 0] = false; tablero[0, 0] = false; tablero[0, 0] = false; tablero[0, 0] = false; tablero[0, 0] = false; tablero[0, 0] = false; tablero[0, 0] = false; tablero[0, 0] = false; tablero[0, 0] = false;
-            tablero[0, 0] = false; tablero[0, 0] = false; tablero[0, 0] = false; tablero[0, 0] = false; tablero[0, 0] = false; tablero[0, 0] = false; tablero[0, 0] = false; tablero[0, 0] = false; tablero[0, 0] = false; tablero[0, 0] = false;
-            tablero[0, 0] = false; tablero[0, 0] = false; tablero[0, 0] = false; tablero[0, 0] = false; tablero[0, 0] = false; tablero[0, 0] = false; tablero[0, 0] = false; tablero[0, 0] = false; tablero[0, 0] = false; tablero[0, 0] = false;
-            tablero[0, 0] = false; tablero[0, 0] = false; tablero[0, 0] = false; tablero[0, 0] = false; tablero[0, 0] = false; tablero[0, 0] = false; tablero[0, 0] = false; tablero[0, 0] = false; tablero[0, 0] = false; tablero[0, 0] = false;
-            tablero[0, 0] = false; tablero[0, 0] = false; tablero[0, 0] = false; tablero[0, 0] = false; tablero[0, 0] = false; tablero[0, 0] = false; tablero[0, 0] = false; tablero[0, 0] = false; tablero[0, 0] = false; tablero[0, 0] = false;
-            tablero[0, 0] = false; tablero[0, 0] = false; tablero[0, 0] = false; tablero[0, 0] = false; tablero[0, 0] = false; tablero[0, 0] = false; tablero[0, 0] = false; tablero[0, 0] = false; tablero[0, 0] = false; tablero[0, 0] = false;
-            tablero[0, 0] = false; tablero[0, 0] = false; tablero[0, 0] = false; tablero[0, 0] = false; tablero[0, 0] = false; tablero[0, 0] = false; tablero[0, 0] = false; tablero[0, 0] = false; tablero[0, 0] = false; tablero[0, 0] = false;
-            tablero[0, 0] = false; tablero[0, 0] = false; tablero[0, 0] = false; tablero[0, 0] = false; tablero[0, 0] = false; tablero[0, 0] = false; tablero[0, 0] = false; tablero[0, 0] = false; tablero[0, 0] = false; tablero[0, 0] = false;
-            tablero[0, 0] = false; tablero[0, 0] = false; tablero[0, 0] = false; tablero[0, 0] = false; tablero[0, 0] = false; tablero[0, 0] = false; tablero[0, 0] = false; tablero[0, 0] = false; tablero[0, 0] = false; tablero[0, 0] = false;
-            tablero[0, 0] = false; tablero[0, 0] = false; tablero[0, 0] = false; tablero[0, 0] = false; tablero[0, 0] = false; tablero[0, 0] = false; tablero[0, 0] = false; tablero[0, 0] = false; tablero[0, 0] = false; tablero[0, 0] = false;
-
+            bool[,] tablero = new bool[5, 5];
+            tablero[0, 0] = false; tablero[0, 1] = false; tablero[0, 2] = false; tablero[0, 3] = false; tablero[0, 4] = false;
+            tablero[1, 0] = false; tablero[1, 1] = false; tablero[1, 2] = false; tablero[1, 3] = false; tablero[1, 4] = false;
+            tablero[2, 0] = false; tablero[2, 1] = false; tablero[2, 2] = false; tablero[2, 3] = false; tablero[2, 4] = false;
+            tablero[3, 0] = false; tablero[3, 1] = false; tablero[3, 2] = false; tablero[3, 3] = false; tablero[3, 4] = false;
+            tablero[4, 0] = false; tablero[4, 1] = false; tablero[4, 2] = false; tablero[4, 3] = false; tablero[4, 4] = false;
             return tablero;
         }
     }
