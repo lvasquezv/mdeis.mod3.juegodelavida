22091289f97eadc8752945b516a2a8f772af7aa9
Luis Rolando Vasquez Velasquez <vasquez.luisrolando@gmail.com>
2019-08-17 01:24:58 -0400

    P04.02: CelulaMuerta_conTresVecinasVivas_Vive : Desarrollo

--- JuegoDeLaVida/JuegoDeLaVidaLib/Juego.cs
+++ JuegoDeLaVida/JuegoDeLaVidaLib/Juego.cs
@@ -9,23 +9,29 @@ namespace JuegoDeLaVidaLib
         public const char TIPO_VIVA_CONMENOSDEDOSVECINASVIVAS_MUERE = 'A';
         public const char TIPO_VIVA_CONDOSTRESVECINASVIVAS_VIVE = 'B';
         public const char TIPO_VIVA_CONMASDETRESVECINASVIVAS_MUERE = 'C';
+        public const char TIPO_MUERTA_CONTRESVECINASVIVAS_VIVE = 'D';
+        public const char TIPO_MUERTA_CONDIFERENTEDETRESVECINASVIVAS_MUERE = 'E';
 
-        public bool[,] generar(bool[,] tableroBase, int posX, int posY)
+        public bool[,] generar(bool[,] tableroBase, int posX, int posY, bool estado)
         {
             bool[,] tablero = (bool[,])tableroBase.Clone();
-            char tipo = obtenerTipo(tableroBase, posX, posY);
+            char tipo = obtenerTipo(tableroBase, posX, posY, estado);
             if (tipo.Equals(TIPO_VIVA_CONMENOSDEDOSVECINASVIVAS_MUERE))
                 tablero[posX, posY] = false;
             if (tipo.Equals(TIPO_VIVA_CONDOSTRESVECINASVIVAS_VIVE))
                 tablero[posX, posY] = true;
             if (tipo.Equals(TIPO_VIVA_CONMASDETRESVECINASVIVAS_MUERE))
                 tablero[posX, posY] = false;
-            
+            if (tipo.Equals(TIPO_MUERTA_CONTRESVECINASVIVAS_VIVE))
+                tablero[posX, posY] = true;
+            if (tipo.Equals(TIPO_MUERTA_CONDIFERENTEDETRESVECINASVIVAS_MUERE))
+                tablero[posX, posY] = false;
             return tablero;
         }
-        public char obtenerTipo(bool[,] tableroBase, int posX, int posY)
+        public char obtenerTipo(bool[,] tableroBase, int posX, int posY, bool estado)
         {
             int cantidadVecinaViva = 0;
+
             if (posX > 0 && tableroBase[posX - 1, posY]) cantidadVecinaViva++;
             if (posX < tableroBase.GetLength(1)-1 && tableroBase[posX + 1, posY]) cantidadVecinaViva++;
             if (posY > 0 && tableroBase[posX, posY-1]) cantidadVecinaViva++;
@@ -37,9 +43,18 @@ namespace JuegoDeLaVidaLib
             if (posX < tableroBase.GetLength(1) - 1 && posY > 0 && tableroBase[posX + 1, posY - 1]) cantidadVecinaViva++;
             if (posX < tableroBase.GetLength(1) - 1 && posY < tableroBase.GetLength(0) - 1 && tableroBase[posX + 1, posY + 1]) cantidadVecinaViva++;
 
-            if (cantidadVecinaViva < 2) return TIPO_VIVA_CONMENOSDEDOSVECINASVIVAS_MUERE;
-            else if (cantidadVecinaViva < 4) return TIPO_VIVA_CONDOSTRESVECINASVIVAS_VIVE;
-            else return TIPO_VIVA_CONMASDETRESVECINASVIVAS_MUERE;
+            if (estado)
+            {
+                if (cantidadVecinaViva < 2) return TIPO_VIVA_CONMENOSDEDOSVECINASVIVAS_MUERE;
+                else if (cantidadVecinaViva < 4) return TIPO_VIVA_CONDOSTRESVECINASVIVAS_VIVE;
+                else return TIPO_VIVA_CONMASDETRESVECINASVIVAS_MUERE;
+            }
+            else
+            {
+                if (cantidadVecinaViva == 3) return TIPO_MUERTA_CONTRESVECINASVIVAS_VIVE;
+                else return TIPO_MUERTA_CONDIFERENTEDETRESVECINASVIVAS_MUERE;
+            }
+            
         }
     }
 }
--- JuegoDeLaVida/JuegoDeLaVidaPruebas/JuegoPruebas.cs
+++ JuegoDeLaVida/JuegoDeLaVidaPruebas/JuegoPruebas.cs
@@ -19,7 +19,7 @@ namespace JuegoDeLaVidaPruebas
             tablero[3, 0] = false; tablero[3, 1] = false; tablero[3, 2] = false; tablero[3, 3] = false; tablero[3, 4] = false;
             tablero[4, 0] = false; tablero[4, 1] = false; tablero[4, 2] = false; tablero[4, 3] = false; tablero[4, 4] = false;
             // Hay una celula viva en la posicion 1,1.
-            bool[,] tableroActual = juego.generar(tablero, 1, 1);
+            bool[,] tableroActual = juego.generar(tablero, 1, 1, true);
             bool[,] tableroEsperado = new bool[5, 5];
             tableroEsperado[0, 0] = false; tableroEsperado[0, 1] = false; tableroEsperado[0, 2] = false; tableroEsperado[0, 3] = false; tableroEsperado[0, 4] = false;
             tableroEsperado[1, 0] = false; tableroEsperado[1, 1] = false; tableroEsperado[1, 2] = false; tableroEsperado[1, 3] = false; tableroEsperado[1, 4] = false;
@@ -41,9 +41,9 @@ namespace JuegoDeLaVidaPruebas
             tablero[3, 0] = false; tablero[3, 1] = false; tablero[3, 2] = false; tablero[3, 3] = true; tablero[3, 4] = false;
             tablero[4, 0] = false; tablero[4, 1] = false; tablero[4, 2] = false; tablero[4, 3] = false; tablero[4, 4] = false;
             // Hay una celula viva en la posicion 2,3  que tiene dos vecinas vivas : 1,3  y  3,3
-            bool[,] tableroActual = juego.generar(tablero, 2, 3);
-            tableroActual = juego.generar(tableroActual, 1, 3);
-            tableroActual = juego.generar(tableroActual, 3, 3);
+            bool[,] tableroActual = juego.generar(tablero, 2, 3, true);
+            tableroActual = juego.generar(tableroActual, 1, 3, true);
+            tableroActual = juego.generar(tableroActual, 3, 3, true);
             bool[,] tableroEsperado = new bool[5, 5];
             tableroEsperado[0, 0] = false; tableroEsperado[0, 1] = false; tableroEsperado[0, 2] = false; tableroEsperado[0, 3] = false; tableroEsperado[0, 4] = false;
             tableroEsperado[1, 0] = false; tableroEsperado[1, 1] = false; tableroEsperado[1, 2] = false; tableroEsperado[1, 3] = false; tableroEsperado[1, 4] = false;
@@ -65,14 +65,36 @@ namespace JuegoDeLaVidaPruebas
             tablero[3, 0] = false; tablero[3, 1] = false; tablero[3, 2] = false; tablero[3, 3] = true; tablero[3, 4] = false;
             tablero[4, 0] = false; tablero[4, 1] = false; tablero[4, 2] = false; tablero[4, 3] = false; tablero[4, 4] = false;
             // Hay una celula viva en la posicion 2,3  que tiene dos vecinas vivas : 1,3  y  3,3
-            bool[,] tableroActual = juego.generar(tablero, 2, 3);
-            tableroActual = juego.generar(tableroActual, 1, 3);
-            tableroActual = juego.generar(tableroActual, 3, 3);
-            tableroActual = juego.generar(tableroActual, 2, 4);
+            bool[,] tableroActual = juego.generar(tablero, 2, 3, true);
+            tableroActual = juego.generar(tableroActual, 1, 3, true);
+            tableroActual = juego.generar(tableroActual, 3, 3, true);
+            tableroActual = juego.generar(tableroActual, 2, 4, true);
             bool[,] tableroEsperado = new bool[5, 5];
             tableroEsperado[0, 0] = false; tableroEsperado[0, 1] = false; tableroEsperado[0, 2] = false; tableroEsperado[0, 3] = false; tableroEsperado[0, 4] = false;
             tableroEsperado[1, 0] = false; tableroEsperado[1, 1] = false; tableroEsperado[1, 2] = false; tableroEsperado[1, 3] = true; tableroEsperado[1, 4] = false;
             tableroEsperado[2, 0] = false; tableroEsperado[2, 1] = false; tableroEsperado[2, 2] = true; tableroEsperado[2, 3] = false; tableroEsperado[2, 4] = true;
+            tableroEsperado[3, 0] = false; tableroEsperado[3, 1] = false; tableroEsperado[3, 2] = false; tableroEsperado[3, 3] = true; tableroEsperado[3, 4] = false;
+            tableroEsperado[4, 0] = false; tableroEsperado[4, 1] = false; tableroEsperado[4, 2] = false; tableroEsperado[4, 3] = false; tableroEsperado[4, 4] = false;
+            // Tablero esperado, la celula unica viva se deberia morir.
+
+            Assert.AreEqual(tableroEsperado, tableroActual);
+        }
+        [Test]
+        public void CelulaMuerta_conTresVecinasVivas_Vive()
+        {
+            JuegoDeLaVidaLib.Juego juego = new JuegoDeLaVidaLib.Juego();
+            bool[,] tablero = new bool[5, 5];
+            tablero[0, 0] = false; tablero[0, 1] = false; tablero[0, 2] = false; tablero[0, 3] = false; tablero[0, 4] = false;
+            tablero[1, 0] = false; tablero[1, 1] = false; tablero[1, 2] = false; tablero[1, 3] = true; tablero[1, 4] = false;
+            tablero[2, 0] = false; tablero[2, 1] = false; tablero[2, 2] = true; tablero[2, 3] = true; tablero[2, 4] = true;
+            tablero[3, 0] = false; tablero[3, 1] = false; tablero[3, 2] = false; tablero[3, 3] = true; tablero[3, 4] = false;
+            tablero[4, 0] = false; tablero[4, 1] = false; tablero[4, 2] = false; tablero[4, 3] = false; tablero[4, 4] = false;
+            // Hay una celula viva en la posicion 2,3  que tiene dos vecinas vivas : 1,3  y  3,3
+            bool[,] tableroActual = juego.generar(tablero, 1, 2, false);
+            bool[,] tableroEsperado = new bool[5, 5];
+            tableroEsperado[0, 0] = false; tableroEsperado[0, 1] = false; tableroEsperado[0, 2] = false; tableroEsperado[0, 3] = false; tableroEsperado[0, 4] = false;
+            tableroEsperado[1, 0] = false; tableroEsperado[1, 1] = false; tableroEsperado[1, 2] = true; tableroEsperado[1, 3] = true; tableroEsperado[1, 4] = false;
+            tableroEsperado[2, 0] = false; tableroEsperado[2, 1] = false; tableroEsperado[2, 2] = true; tableroEsperado[2, 3] = true; tableroEsperado[2, 4] = true;
             tableroEsperado[3, 0] = false; tableroEsperado[3, 1] = false; tableroEsperado[3, 2] = false; tableroEsperado[3, 3] = true; tableroEsperado[3, 4] = false;
             tableroEsperado[4, 0] = false; tableroEsperado[4, 1] = false; tableroEsperado[4, 2] = false; tableroEsperado[4, 3] = false; tableroEsperado[4, 4] = false;
             // Tablero esperado, la celula unica viva se deberia morir.
